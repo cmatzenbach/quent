95f045d3b444a9c96827a0c99de1aa96a98d9320
Chris Matzenbach <cmatzenbach@nvidia.com>
2026-03-25 14:50:10 -0500

    chore(legal): add Apache 2.0 license, SPDX headers, and DCO contribution guide (#64)
    Addresses all items listed out in Cary's [slack message](https://nvidia.slack.com/archives/CE71RAA6Q/p1773862906048349?thread_ts=1771949538.963159&cid=CE71RAA6Q) required for open sourcing a repository.
    
    Authors:
      - Chris Matzenbach (https://github.com/cmatzenbach)
    
    Approvers:
      - Joe O'Hallaron (https://github.com/johallar)
    
    URL: https://github.com/rapidsai/quent/pull/64

--- ui/src/components/resource-tree/ResourceGroupRow.tsx
+++ ui/src/components/resource-tree/ResourceGroupRow.tsx
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 import { ResourceGroup } from '~quent/types/ResourceGroup';
 import { InlineSelector } from './InlineSelector';
 
--- ui/src/lib/timeline.utils.ts
+++ ui/src/lib/timeline.utils.ts
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 import { TimelineSeries, TimelineMark } from '@/components/timeline/types';
 import { TreeTableItem } from '@/components/resource-tree/types';
 import { formatQuantity } from '@/services/formatters';
